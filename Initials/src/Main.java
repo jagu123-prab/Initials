6d459b5d3f1c4cfceecf07f79f5606d9d8a95a08
Jagath-Prabhath <jagathprabhath@gmail.com>
2025-08-14 10:25:25 -0700

    geznkxz

--- Initials/src/Main.java
+++ Initials/src/Main.java
@@ -3,63 +3,43 @@ import kareltherobot.*;
 public class Main implements Directions{
   public static void main(String[] args) {
     World.setVisible(true);// allows us to see the run
-World.setDelay(1);
+    World.setDelay(1);
+    World.setSize(11,11);
+
+
+
     // The line below creates a Robot that we will refer to as r.  
     // Find out what the numbers and direction do!
-    Robot r = new Robot(1,2,East,9);
-		
+    Robot r = new Robot(1,6,North,100);
+		for (int i=0; i<5;i++){
+      r.putBeeper();
+      r.move();
+      r.turnLeft();
+      r.move();
+      r.putBeeper();
+      r.turnLeft();
+      r.turnLeft();
+      r.turnLeft();
+    }
+    for (int i=0; i<5;i++){
+      r.putBeeper();
+      r.turnLeft();
+      r.turnLeft();
+      r.turnLeft();
+      r.move();
+      r.turnLeft();
+      r.move();
+    }
+    for (int i=0; i<5; i++)
+
+    
+
+
+
     // examples of commands you can invoke on a Robot
     r.move();// move one step in the direction it is facing
-    r.turnLeft();
-    r.putBeeper();
-    r.turnLeft();
-    r.turnLeft();
-    r.turnLeft();
-    r.move();
-    r.turnLeft();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.turnLeft();
-    r.turnLeft();
-    r.turnLeft();
-    r.move();
-    r.move();
-    r.turnLeft();
-    r.turnLeft();
-    r.turnLeft();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper(); 
+   
 
-	r.putBeeper();
 
   }
 }
